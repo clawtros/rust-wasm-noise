fe10f41e8d0dbcaf1731eb4f684fae026993a29a
Adam Benzan <adam@foursixty.com>
2020-02-16 10:14:46 -0500

    fix some merges

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 // mod utils;
+use noise::{NoiseFn, SuperSimplex};
 use wasm_bindgen::prelude::*;
-use noise::{NoiseFn, Perlin};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -14,41 +14,50 @@ pub struct NoiseGrid {
     height: u32,
     z: f64,
     scale: f64,
-    speed: f64,
-    noise: Perlin,
-    cells: Vec<f64>
+    noise: SuperSimplex,
+    image_data: Vec<u8>,
 }
 
 #[wasm_bindgen]
 impl NoiseGrid {
-    pub fn new(width: u32, height: u32, speed: f64, scale: f64) -> NoiseGrid {
-        let noise = Perlin::new();
+    pub fn new(width: u32, height: u32, scale: f64) -> NoiseGrid {
+        let noise = SuperSimplex::new();
         let z = 0.0;
-        let cells = (0..width * height).map(|_i| {return 0.0;}).collect();
+        let image_data = vec![];
         return NoiseGrid {
             width,
             height,
             z,
-            speed,
             scale,
             noise,
-            cells            
-        }
+            image_data,
+        };
     }
 
-    pub fn cells(&self) -> *const f64 {
-        return self.cells.as_ptr();
+    pub fn set_scale(&mut self, scale: f64) {
+        self.scale = scale;
     }
-   
-    pub fn tick(&mut self) {
-        let _z = self.z + self.speed;
-        let cells = (0..self.width * self.height)
-            .map(|i| {
-                let x = i % self.width;
-                let y = i / self.height;
-                return self.noise.get([f64::from(x) * self.scale, f64::from(y) * self.scale, _z]);
-            }).collect();
+
+    pub fn image_data(&self) -> *const u8 {
+        return self.image_data.as_ptr();
+    }
+
+    pub fn tick(&mut self, speed: f64) {
+        let _z = self.z + speed;
+        let _imgdata = (0..self.width * self.height)
+            .flat_map(|i| {
+                let n = self.noise.get([
+                    ((i % self.width + self.width) as f64) * self.scale,
+                    ((i / self.height + self.height) as f64) * self.scale,
+                    _z,
+                ]);
+                let color: Rgb<u8> = Hsv::new(Deg(n * 360.0), 0.9, 1.9).to_rgb();
+                //return vec![color.r, color.g, color.b, 255];
+                let s = ((color.r + color.g + color.b) / 3 > 32) as u8 * 255;
+                //let s = (n > 0.) as u8 * 255;
+                return vec![s, s, s, 255];
+            })
+            .collect();
         self.z = _z;
-        self.cells = cells;
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,35 +1,66 @@
 import {NoiseGrid} from "wasm-noise";
 import {memory} from "wasm-noise/wasm_noise_bg";
+import {fps} from "./fps";
 
-const width = parseInt(window.innerWidth / 15) ;
-
-const height = parseInt(window.innerHeight / 15);
-const grid = NoiseGrid.new(width, height, 0.025, 0.025);
+const state = {
+  currentScale: 0.00465
+}
+const smod = 4;
+const width = parseInt(window.innerWidth / smod);
+const height = parseInt(window.innerHeight / smod);
+const initialScale = 0.00625;
+const grid = NoiseGrid.new(width, height, initialScale);
 const canvas = document.getElementById("render-canvas");
-const fps = document.getElementById("fps");
 canvas.width = width;
 canvas.height = height;
 const ctx = canvas.getContext("2d");
 const cellsPtr = grid.cells();
 
 const drawCells = function() {
-  const cells = new Float64Array(memory.buffer, cellsPtr, width * height);  
-  for (var i = 0; i < cells.length; i++) {
-    const [x, y] = [i % width, i / width];
-    ctx.fillStyle = `hsl(${parseInt((cells[i] + 0.5) * 360)}deg, 70%, 50%)`;
-    ctx.fillRect(x, y, 1, 1);
-  }
+  const imageData = ctx.getImageData(0,0,width, height);
+  imageData.data.set(new Uint8Array(memory.buffer, grid.image_data(), width * height * 4));
+  ctx.putImageData(imageData, 0, 0);
 }
 
-let lastDelta = 0;
-const renderLoop = function (delta) {
-  requestAnimationFrame(renderLoop);
+const handleSuccess = function(stream) {
+  const context = new AudioContext();
+  const source = context.createMediaStreamSource(stream);
+  const analyser = context.createAnalyser();
+  source.connect(analyser);
+  analyser.fftSize = 64;
+  state.dataArray = new Uint8Array(analyser.frequencyBinCount);
+  state.analyser = analyser;
+};
 
-  grid.tick();
+var steps = 0;
+const renderLoop = function () {
+  //fps.render();
+  //  grid.set_scale(initialScale - (Math.sin(steps++ * 0.0125) + 5.5) / 100);
+  ctx.fillStyle="#ffffff";
+  ctx.fillRect(0, 0, width, height);
+
   drawCells();
-
-  fps.innerText = (1000 / (delta - lastDelta)).toFixed(1) + "fps"; 
-  lastDelta = delta;
+  if (state.analyser) {
+    ctx.fillStyle="#ff0000";
+    state.analyser.getByteTimeDomainData(state.dataArray);
+    ctx.moveTo(0, height / 2);
+    let sum = 0;
+    for (var i = 0, l = state.dataArray.length; i  < l; i++) {
+      let p = state.dataArray[i];
+      sum += p;
+      let barwidth = width / l;
+      //ctx.lineTo(i * barwidth, p);
+    }
+    let v = (sum / state.dataArray.length - 127) / 1000;
+    grid.tick(v);
+    console.log(v)
+    //ctx.strokeStyle="#ff0000";
+    //ctx.stroke();
+  }
+  requestAnimationFrame(renderLoop);
 }
 
 renderLoop();
+
+navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+         .then(handleSuccess);
